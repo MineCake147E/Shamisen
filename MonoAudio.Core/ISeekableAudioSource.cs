e413380c71a12809499e627e5cf9db1b7c3c3147
MineCake147E <MineCake1.4.7@outlook.com>
2019-08-07 15:38:31 +0900

    +Added ISeekableAudioSource

---
+++ MonoAudio.Core/ISeekableAudioSource.cs
@@ -0,0 +1,12 @@
+﻿namespace MonoAudio
+{
+    /// <summary>
+    /// Defines a base infrastructure of a seekable audio source.
+    /// </summary>
+    public interface ISeekableAudioSource
+    {
+        
+        long Length { get; }
+        long Position { get; set; }
+    }
+}
