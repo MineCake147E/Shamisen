010435a2ff787a152e21bd47144580c9de695ff3
MineCake147E <minecake1.4.7@outlook.com>
2019-09-16 19:24:28 +0900

    + Added interfaces for Audio Devices and Enumeration for them.

---
+++ MonoAudio.Core/IO/IAudioOutputDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoAudio.IO
+{
+    /// <summary>
+    /// Defines a base structure of audio output device.
+    /// </summary>
+    public interface IAudioOutputDevice : IAudioDevice
+    {
+        /// <summary>
+        /// Indicates whether the audio output device supports a particular stream format.
+        /// </summary>
+        /// <param name="format">The format to judge the availability.</param>
+        /// <returns><c>true</c> if succeeded and the audio device supports the specified stream format.</returns>
+        bool IsFormatSupported(WaveFormat format);
+    }
+}
