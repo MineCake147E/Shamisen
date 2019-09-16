d2e9b6d5ac56ad71ea5826834e13b939c5350134
MineCake147E <minecake1.4.7@outlook.com>
2019-09-16 19:21:29 +0900

    +Added concept documents.

---
+++ MonoAudio.Core/Concepts/ConceptAttributes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonoAudio.Concepts
+{
+    /// <summary>
+    /// Represents a kind of concepts.
+    /// </summary>
+    public enum ConceptKind
+    {
+        /// <summary>
+        /// Invalid.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The concept of Units.
+        /// </summary>
+        Unit,
+    }
+
+    /// <summary>
+    /// Defines a concept.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    public sealed class ConceptAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ConceptAttribute"/>.
+        /// </summary>
+        /// <param name="kind">The kind of concept.</param>
+        public ConceptAttribute(ConceptKind kind) => Kind = kind;
+
+        /// <summary>
+        /// The kind of concept.
+        /// </summary>
+        public ConceptKind Kind { get; }
+    }
+}
